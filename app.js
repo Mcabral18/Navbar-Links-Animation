28f5480c2f400ac9aeb2f6526ebbed9b2999337b
Your Name <you@example.com>
2021-01-12 22:06:13 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+const Links = document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click", ()=> {
+    navLinks.classList.toggle("nav-active");
+    burger.classList.toggle("active");
+Links.forEach((link,index)=>{
+    // link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+    if(link.style.animation){
+        link.style.animation = '';
+    }
+    else{
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    }
+})
+});
+
+
+
+//// other way
+
+// const navSlide = () => {
+//     const burger = document.querySelector(".burger");
+//     const navLinks = document.querySelector(".nav-links");
+//     const Links = document.querySelectorAll(".nav-links li")
+
+//     burger.addEventListener("click", ()=> {
+//         //toggle menu
+//         navLinks.classList.toggle("nav-active");
+//         burger.classList.toggle("active");
+//         //animated navLinks
+//         Links.forEach((link,index)=>{
+//             // link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+//             if(link.style.animation){
+//                 link.style.animation = '';
+//             }
+//             else{
+//                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+//             }
+//         })
+//     });
+
+// }
+
+// navSlide();
